53eb1c1ba7c8a9b7328a8aed7846acad43fdd852
VirtualDruid <virtualdruid70@gmail.com>
2024-11-14 14:54:00 +0800

    GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@
 import { Analytics } from '@vercel/analytics/react';
 import ReactGA from 'react-ga4';
 //const inter = Inter({ subsets: ["latin"] });
-ReactGA.initialize("G-0YYSXZ22C2");
+ReactGA.initialize("G-MB90LM6CT6");
 
 export default function RootLayout({
   children,
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" style={{backgroundColor:"#B61316", height:"100%"}}>
-      <title>GBC</title>
+    <html lang="en" style={{backgroundColor:"#F5ACC7", height:"100%"}}>
+      <title>Bocchi</title>
       <body>{children}
         <Analytics/>
       </body>
